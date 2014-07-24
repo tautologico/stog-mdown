c2684454d7ca14c7674f17233b714e059d806eba
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-07-23 23:04:19 -0300

    Implemented markdown transformation using omd

--- src/stog_mdown.ml
+++ src/stog_mdown.ml
@@ -1,5 +1,12 @@
 let fun_mdown stog env args subs = 
-  (stog, [])
+  let input = 
+    match subs with
+      [ Xtmpl.D text ] -> text
+    | _ -> String.concat "" (List.map Xtmpl.string_of_xml subs)
+  in
+  let mdown = Omd.of_string input in
+  let html = Omd.to_html mdown in
+  Xtmpl.apply_to_string stog env html 
 
 let () = Stog_plug.register_html_base_rule ("", "mdown") fun_mdown
 
